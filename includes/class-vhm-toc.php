79e99561ea88b634a109dd3bfd36e61d53b2ee96
Viktor H. Morales <viktorhugomorales@gmail.com>
2020-04-22 18:19:38 -0300

    UPDATE

--- includes/class-vhm-toc.php
+++ includes/class-vhm-toc.php
@@ -188,8 +188,6 @@ class Vhm_Toc {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 		
-		$this->loader->add_action( 'wp_footer', $plugin_public, 'footer_script' );
-
 		$this->loader->add_action( 'init', $plugin_public, 'register_shortcodes');
 	}
 
